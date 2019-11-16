57eee07b9e44e7043ff50ffea1995589c00c03bf
LapinoLapidus <redactyl@protonmail.ch>
2019-11-16 23:46:10 +0100

    More npm-friendly

---
+++ lib/index.d.ts
@@ -0,0 +1,55 @@
+import { Message, TextChannel, User } from "discord.js";
+import { Answer } from "./interfaces/Answer";
+import { Question } from "./interfaces/Question";
+export { Question } from "./interfaces/Question";
+export { Answer } from "./interfaces/Answer";
+export declare class Disquirer {
+    get questions(): Question[];
+    set questions(value: Question[]);
+    private static reactionNumbers;
+    private _questions;
+    private currentQuestion;
+    private originalQuestion;
+    /**
+     * The constructor used to create a Disquirer object.
+     */
+    constructor(questions: Question[]);
+    /**
+     * Adds corresponding reactions for a single question.
+     * @param msg
+     * @param question
+     */
+    addReactions: (msg: Message, question: Question) => Promise<void>;
+    /**
+     * Get the reactions (unicode) for a given question.
+     * @param question
+     */
+    getReactions: (question: Question) => string[];
+    /**
+     * Creates a prompt and returns the an answer array when done with asking all questions.
+     * This is probably the method you're gonna use.
+     * @param channel
+     * @param target
+     * @returns promise - Resolves with an array of answers. Rejects with an error.
+     */
+    createPrompt: (channel: TextChannel, target: User) => Promise<Answer[]>;
+    /**
+     * Returns possible answers for a question, if possible.
+     * @param bracketStyle Whether to use the bracket style [Y/n] for the possible answers or newLine (false).
+     * @param question The question to get the possible answers for.
+     */
+    getPossibleAnswers: (bracketStyle: boolean, question: Question) => string;
+    private askQuestion;
+    private controllerHandler;
+    private handleReactionEvent;
+    /**
+     * Handler for the message event.
+     * @param msg
+     * @param question
+     */
+    private handleMessageEvent;
+    /**
+     * Shifts a question from the questions array, and returns it.
+     */
+    private getQuestionToSend;
+}
---
+++ lib/interfaces/Question.d.ts
@@ -0,0 +1,50 @@
+export interface Question {
+    /**
+     * The question.
+     */
+    text: string;
+    /**
+     * An array of possible answers for the question.
+     * Required for a question with reaction for reactionMethod.
+     * When using reactionMethod = "reaction", put a reaction here.
+     */
+    possibleAnswers?: string[];
+    /**
+     * The style to display the answers.
+     * For example: bracket:
+     * Do you want to.. [Y/n]
+     * For example: newLine:
+     * Do you want to..
+     * 0: Y
+     * 1: n
+     */
+    /**
+     * Makes it possible for the user to deviate from possible answers and send their own text.
+     * Whether or not a possible answer is required.
+     * Only has effect when using reactionMethod = "text"
+     * @default true
+     */
+    possibleAnswerRequired?: boolean;
+    /**
+     * Sets the style to either "bracket" or "newLine".
+     * Bracket: [Y/n]
+     * Newline:
+     * 1: a
+     * 2: b
+     * 3: c
+     */
+    style: "bracket" | "newLine";
+    /**
+     * Whether you want the user to type out their response (text) or click on an emoji (reaction).
+     */
+    reactionMethod: "text" | "reaction";
+    /**
+     * The datatype which will be checked when the question is answered.
+     */
+    dataType?: string;
+    /**
+     * If datatype is `number`, then it will be checked if it's bigger than or equal to the length.
+     * If datatype is `string`, then it will compare the string length to see if it's bigger than or equal to the length.
+     */
+    length?: number;
+}
